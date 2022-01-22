0248df082a960cf33dca481418943bd4269aff01
Reynaldi Julio Setiabudi <reynaldijuliosetia@icloud.com>
2022-01-22 13:02:45 +0700

    FEATURE(medes-ui) : Remove shared folder & use custom lib

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { MdsFilterCheckboxModule, MdsPipesModule } from 'mds-ui';
+import { MdsFilterCheckboxModule, MdsPipesModule } from 'medes-ui';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
