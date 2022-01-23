f13d102f5e703ca92ebd4092537526fdde644686
Reynaldi Julio Setiabudi <reynaldijuliosetia@icloud.com>
2022-01-23 15:07:42 +0700

    FEATURE(PrismJs) : Add code Prism highlighter Library

--- src/app/filter-checkbox/filter-checkbox.component.ts
+++ src/app/filter-checkbox/filter-checkbox.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HighlightPrismService } from '../services/highlight-prism.service';
 
 @Component({
   selector: 'app-filter-checkbox',
@@ -6,6 +7,49 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./filter-checkbox.component.scss']
 })
 export class FilterCheckboxComponent implements OnInit {
+samplehtml: string = `<pre><code class="language-html">&lt;div class=&quot;group&quot;&gt;
+  &lt;span&gt;Content&lt;span&gt;
+&lt;/div&gt;</code></pre>`
+samplescss: string = `<pre><code class="language-css">.body{
+  background: #f9f9f9;
+  color: #ddd
+}</code></pre>`;
+samplejs: string = `<pre><code class="language-javascript">function GetData(values){
+  console.log(values);
+}</pre>`;
+samplets: string = `<pre><code class="language-typescript">import { Component, OnInit, AfterViewChecked } from '@angular/core';
+import { HighlightService } from '../shared/utils/highlight.service';
+
+@Component({
+  selector: 'mds-library',
+  templateUrl: './mds-library.component.html',
+  styleUrls: ['./mds-library.component.scss']
+})
+export class MdsLibraryComponent implements OnInit, AfterViewChecked {
+
+  constructor(
+    ...
+    private highlightService: HighlightService) {
+  }
+  ngOnInit() {
+    ...
+  }
+}</code></pre>`;
+  
+  private isLoaded: boolean = false;
+
+  constructor(private highlightService: HighlightPrismService) {}
+
+  ngAfterViewChecked() {
+    if (!this.isLoaded) {
+      this.highlightService.highlightAll();
+      this.isLoaded = true;
+    }
+  }
+
+  ngOnInit(): void {
+  }
+
   sampledata = [
     {category: 'eastern food', type:'food'},
     {category: 'western food', type:'drink'},
@@ -17,11 +61,6 @@ export class FilterCheckboxComponent implements OnInit {
   filteritems = {
     category:[ 'eastern food', 'western food'],
     type:['food', 'drink']
-  }
-  
-  constructor() { }
-
-  ngOnInit(): void {
   }
 
 }
