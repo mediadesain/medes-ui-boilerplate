882912b4be81f1a9d1aa6ecb41742ab51a33c0a8
Reynaldi Julio Setiabudi <reynaldijuliosetia@icloud.com>
2022-01-26 01:38:59 +0700

    UX/UI(medes-aside) : Add aside menu list

--- src/app/shared/aside/aside.component.ts
+++ src/app/shared/aside/aside.component.ts
@@ -6,7 +6,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./aside.component.scss']
 })
 export class AsideComponent implements OnInit {
-
+  menu: any[] = [
+    {
+      'title': 'Introduction',
+      'links': [
+        {'text': 'What', 'url': '/'},
+        {'text': 'How to Install', 'url': '/'},
+        {'text': 'List Sub 3', 'url': '/'}
+      ]
+    },{
+      'title': 'Components',
+      'links': [
+        {'text': 'Multiple Filter Checkbox', 'url': '/'},
+        {'text': 'PrismJs Highlight', 'url': '/'}
+      ]
+    },{
+      'title': 'Utilities',
+      'links': [
+        {'text': 'String', 'url': '/'},
+        {'text': 'Integer', 'url': '/'},
+        {'text': 'Object', 'url': '/'},
+        {'text': 'Array', 'url': '/'}
+      ]
+    }
+  ]
   constructor() { }
 
   ngOnInit(): void {
