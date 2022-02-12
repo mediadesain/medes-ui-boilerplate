5cce05573523d64cf1d224cad33739763109ef4a
Reynaldi Julio Setiabudi <reynaldijuliosetia@icloud.com>
2022-02-13 00:30:02 +0700

    DOC(detail-utils) : Update documentation MdsDateUtils

--- src/app/demo-pages/utils/date-utils/date-utils.component.ts
+++ src/app/demo-pages/utils/date-utils/date-utils.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { MdsDateUtils } from 'medes-ui';
+// import { MdsDateUtils } from 'medes-ui';
+import { MdsDateUtils } from 'projects/medes-ui/src/public-api';
 
 @Component({
   selector: 'medes-date-utils',
@@ -8,10 +9,16 @@ import { MdsDateUtils } from 'medes-ui';
 })
 export class DateUtilsComponent implements OnInit {
 import = `import { MdsDateUtils } from 'medes-ui';`;
+demo = `MdsDateUtils.untilNow(1644684102000, ['tahun','bulan','hari','jam','menit','detik'], 'lalu') // output ${MdsDateUtils.untilNow(1644684102000, ['tahun','bulan','hari','jam','menit','detik'], 'lalu')}`;
 
   constructor() { }
 
   ngOnInit(): void {
+    const test = 24*60*60*1000;
+    const date = new Date();
+    console.log(
+      MdsDateUtils.untilNow(1644684102000, ['tahun','bulan','hari','jam','menit','detik'], 'lalu')
+    )
   }
 
 }
