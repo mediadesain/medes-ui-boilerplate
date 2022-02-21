463e4756d3213174c702c435d1e7e0deda4fc660
Reynaldi Julio Setiabudi <reynaldijuliosetia@icloud.com>
2022-02-21 23:33:24 +0700

    FIX(navside) : Hide sidebar if routing changing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'medes-root',
@@ -6,4 +8,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+
+  constructor(
+    private router: Router
+  ){
+    this.router.events.pipe(
+      filter( e => e instanceof NavigationEnd)
+    ).subscribe( (navEnd: NavigationEnd) => {
+      document.getElementsByTagName('content')[0].classList.remove('active');
+    });
+  }
 }
