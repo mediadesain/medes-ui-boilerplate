2b73816ee79673a4dc63d289c34a22c02a11a73f
Reynaldi Julio Setiabudi <reynaldijuliosetia@icloud.com>
2022-02-24 00:08:54 +0700

    RELEASE(dist) : Beta version medes-ui@0.0.3-beta.3

--- dist/medes-ui/lib/mds-hightlight-prism/mds-hightlight-prism.component.d.ts
+++ dist/medes-ui/lib/mds-hightlight-prism/mds-hightlight-prism.component.d.ts
@@ -1,13 +1,16 @@
 import { AfterViewInit, ElementRef, OnChanges } from '@angular/core';
 import 'prismjs';
-import 'prismjs/components/prism-css';
+import 'prismjs/components/prism-markup';
 import 'prismjs/components/prism-javascript';
 import 'prismjs/components/prism-java';
-import 'prismjs/components/prism-markup';
 import 'prismjs/components/prism-typescript';
+import 'prismjs/components/prism-css';
 import 'prismjs/components/prism-sass';
 import 'prismjs/components/prism-scss';
 import 'prismjs/components/prism-ruby';
+import 'prismjs/components/prism-swift';
+import 'prismjs/components/prism-sql';
+import 'prismjs/components/prism-python';
 export declare class MdsHightlightPrismComponent implements AfterViewInit, OnChanges {
     codeContent: ElementRef;
     code?: string;
