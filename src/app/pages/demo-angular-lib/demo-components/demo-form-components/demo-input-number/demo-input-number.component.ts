0f239d256c91064f6acff0a31786131a557369c1
Reyaldi Julio Setiabudi <reynaldijuliosetia@icloud.com>
2025-06-13 00:31:21 +0700

    DOC : Update documentation miss attribute

--- src/app/pages/demo-angular-lib/demo-components/demo-form-components/demo-input-number/demo-input-number.component.ts
+++ src/app/pages/demo-angular-lib/demo-components/demo-form-components/demo-input-number/demo-input-number.component.ts
@@ -33,7 +33,7 @@ export class DemoInputNumberComponent {
   // Properties Detail
   tableContent = [
     {attribute: 'value', type: 'boolean', default: '∞', description: 'Value of component itself', version: 'medes-ui@1.15.3 >'},
-    {attribute: 'classNames?', type: 'string', default: '∞', description: 'Put classes to for styling component', version: 'medes-ui@1.15.3 >'},
+    {attribute: 'customClass?', type: 'string', default: '∞', description: 'Put classes to for styling component', version: 'medes-ui@1.15.3 >'},
     {attribute: 'disabled?', type: 'boolean', default: 'false', description: 'Make it disabled. Or make it disabled by parsing custom config', version: 'medes-ui@1.15.3 >'},
     {attribute: 'model?',type: 'MdsFormModel', default: '∞', description: 'Custom configuration of the component itself', version: 'medes-ui@1.15.3 >'}
   ]
